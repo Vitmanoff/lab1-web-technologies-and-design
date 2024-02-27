d452512cdefccd5dc1934463aea3a980a2acd585
Vitmanoff <126625790+Vitmanoff@users.noreply.github.com>
2024-02-28 01:17:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.getElementById('background-video');
+
+    // Устанавливаем громкость (от 0 до 1)
+    video.volume = 0.01; // например, устанавливаем громкость на половину
+
+    // Далее ваш код или обработчики событий
+});
