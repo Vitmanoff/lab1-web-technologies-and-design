e1564753f4f641b904592c1eca6cf2b6d3e8a022
Vitmanoff <126625790+Vitmanoff@users.noreply.github.com>
2024-02-28 10:03:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+   // Функция для отображения выбранной вкладки
+   function showTab(tabName) {
+    // Скрыть все вкладки
+    var tabs = document.getElementsByClassName('content');
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('active');
+    }
+
+    // Отобразить выбранную вкладку
+    document.getElementById(tabName).classList.add('active');
+
+    // Выделить активную вкладку
+    var tabButtons = document.getElementsByClassName('tab');
+    for (var i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].classList.remove('active-tab');
+    }
+    document.querySelector('[onclick="showTab(\'' + tabName + '\')"]').classList.add('active-tab');
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     var video = document.getElementById('background-video');
 
     // Устанавливаем громкость (от 0 до 1)
-    video.volume = 0.01; // например, устанавливаем громкость на половину
-
-    // Далее ваш код или обработчики событий
+    video.volume = 0.01; 
 });
+
+function playVideo() {
+    var video = document.getElementById('background-video');
+    video.play();
+    document.getElementById('play-button').style.display = 'none'; // Скрываем кнопку после воспроизведения
+}
